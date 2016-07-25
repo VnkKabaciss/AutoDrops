20a459048a01cd14508ceedf43ff3e38f34eb06a
VnkKabaciss <robertslcis097@gmail.com>
2016-07-25 19:39:19 +0300

    Add files via upload

---
+++ AutoDrops.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using Rocket.Unturned;
+using Rocket.Unturned.Chat;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using System.Collections.Generic;
+using SDG.Unturned;
+
+namespace AutoDropsTimer
+{
+    public class AutoDrop : RocketPlugin<AutoDropsConfig>
+    {
+        private DateTime lastCalled = DateTime.Now;
+
+        protected override void Load()
+        {
+            Logger.LogWarning("\tPlugin Loaded Successfully");
+            this.checkChat();
+        }
+
+        private void checkChat()
+        {
+            if ((DateTime.Now - this.lastCalled).TotalSeconds > 20)
+            {
+                SendChat();
+                RunDrop();
+                this.lastCalled = DateTime.Now;
+                FixedUpdate();
+            }
+        }
+        public static void SendChat()
+        {
+            UnturnedChat.Say("Automatic airdrop launched! Go catch it!");
+        }
+
+        public void RunDrop()
+        {
+            CommandAirdrop Type
+        }
+
+        public void FixedUpdate()
+        {
+            if ((DateTime.Now - this.lastCalled).TotalSeconds > 1)
+            {
+                checkChat();
+            }
+        }
+    }
+}
---
+++ AutoDropsConfig.cs
@@ -0,0 +1,18 @@
+﻿using Rocket.API;
+
+namespace AutoDropsTimer
+{
+     public class AutoDropsConfig : IRocketPluginConfiguration
+{
+        public bool Enabled;
+        public ushort SendInterval;
+        public string SendMsg;
+
+        public void LoadDefaults()
+        {
+             Enabled = true; 
+             SendInterval = 20; 
+             SendMsg = "Automatic Airdrop Launched! Go catch it!";
+        }
+    }
+}
